0e4973000eba8ec432662e04e42b98210351cf6f
CHOBODI <CHOBODI>
2017-05-22 17:41:21 +0530

    server and app

--- src/app/app.js
+++ src/app/app.js
@@ -1,19 +1,24 @@
-var app = angular.module('myApp', []);
-app.controller('myCtrl', function($scope,$http) {
-   $scope.submit= function(){
-      var data = $.param({
-        weather: JSON.stringify({
-            name: $scope.name,
-            location : $scope.location,
-            temp : $scope.temp,
-            humidity : $scope.humidity,
-            wind : $scope.wind
-           
-        })
-      });
+var app = angular.module('myApp',[]);
+ app.controller('myCtrl',function($scope,$http){    
+ $scope.submit=function(){      
+ 			var data = {
+                    name     : $scope.name, 
+                    location : $scope.location,
+                    temp     : $scope.temp,
+                    humidity : $scope.humidity,
+                    wind     : $scope.wind
+                    
+                }   
+            };      
 
-      $http.post("/data", data).success(function(data, status) {
-        console.log('Data posted successfully');
-      })
-   }
-});
+ 	$http.post('/', $scope.data)
+            .success(function (data, status, headers, config) {
+                    $scope.result = data;
+                    console.log("inserted successfully")
+                })
+                .error(function(data, status, header, config){
+                    $scope.result = "Data: " + status;
+
+
+                });
+     });
--- src/app/server.js
+++ src/app/server.js
@@ -1,23 +1,30 @@
-var express = require('express'); 
-var mysql = require('mysql');
-var app = express();
+var express = require('express');
+   var bodyParser = require('body-parser');
+   var mysql = require('mysql');
+   var app = express();
 
-var connection = mysql.createConnection({
-     host: 'localhost',
-     user: 'root',
-     password: '',
-     database: 'weather'
-});
-connection.connect();
+    app.use(bodyParser.json({limit: '50mb'}));
+    app.use(express.static('public'));
 
-app.post('/data', function(req, res, next){
-   var cope = req.body.params;
-   var query = connection.query('insert into data set?', cope, function(err, result) {
-     if (err) {
-       console.error(err);
-       return res.send(err);
-     } else {
-       return res.send('Ok');
-     }
-});
-app.listen(8080);
+  var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'weather'
+   });
+   connection.connect();
+
+    app.post('/', function(req, res, next) {
+    var cope = req.body;
+    console.log('request received:', req.body);
+   var query = connection.query('insert into data set ?', cope, function (err,result) {
+    if (err) {
+        console.error(err);
+        return res.send(err);
+    } else {
+        return res.send('Ok');
+    }
+    });
+    //res.send('received the data.');
+    });
+    app.listen(3000);
